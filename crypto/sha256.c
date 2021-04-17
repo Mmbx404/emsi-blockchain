8d6d2372a4ad09afbba273a4f7967fa9197fea19
Mmb <medmehdibelghali@gmail.com>
2021-04-17 12:01:32 +0000

    changed if statement i guess?

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,7 +1,7 @@
 #include "hblk_crypto.h"
 
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]) {
-	if (s == null || digest == null) {
+	if (!s || !digest) {
 		return null;
 	} else {
 		SHA256_CTX c;
