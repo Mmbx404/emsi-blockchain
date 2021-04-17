0a6dc9c2fe7d9a7bcd8376b23709cbc0f0e5ebf2
Mmb <medmehdibelghali@gmail.com>
2021-04-17 16:39:19 +0000

    changed sha256 again

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -4,16 +4,7 @@ uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH
 	if (!s) {
 		return NULL;
 	} else {
-		EVP_MD_CTX* evp_md_ctx;
-        const EVP_MD* evp_md;
-        int md_length;
-        evp_md = EVP_sha256();
-        evp_md_ctx = EVP_MD_CTX_create();
-        EVP_DigestInit_ex(evp_md_ctx, evp_md, NULL);
-        EVP_DigestUpdate(evp_md_ctx, s, strlen(s));
-        EVP_DigestFinal_ex(evp_md_ctx, digest, &md_length);
-        EVP_MD_CTX_destroy(evp_md_ctx);
-        EVP_cleanup();
-        return digest;
+        if (s == NULL || digest == NULL) return (NULL);
+        return (SHA256((const unsigned char *)s, len, digest));
 	}
 }
