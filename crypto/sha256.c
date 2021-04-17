d8357fbfdde8724e1a031dc177d285319dba293c
Mmb <medmehdibelghali@gmail.com>
2021-04-17 11:45:29 +0000

    some changes in sha256

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -4,6 +4,6 @@ uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH
 	if (s == null || digest == null) {
 		return null;
 	} else {
-		return SHA256(s, len, digest);
+		return SHA256((const unsigned char *)s, len, digest);
 	}
 }
