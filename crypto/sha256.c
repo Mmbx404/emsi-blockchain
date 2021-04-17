cf4b7922ceb416e1439284355c624d603d93038d
Mmb <medmehdibelghali@gmail.com>
2021-04-17 12:05:18 +0000

    another one

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,7 +1,7 @@
 #include "hblk_crypto.h"
 
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]) {
-	if (!s || !digest) {
+	if (!s) {
 		return null;
 	} else {
 		SHA256_CTX c;
