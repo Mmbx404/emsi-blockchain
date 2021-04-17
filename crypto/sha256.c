8ced5e30166ac7a94d1f60db1d6a3718ada3996d
Mmb <medmehdibelghali@gmail.com>
2021-04-17 12:14:35 +0000

    switched from deprecated version to new one

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -2,12 +2,18 @@
 
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]) {
 	if (!s) {
-		return null;
+		return NULL;
 	} else {
-		SHA256_CTX c;
-		SHA256_Init(&c);
-		SHA256_Update(&c, s, len);
-		SHA256_Final(digest, &c);
-		return digest;
+		EVP_MD_CTX* evp_md_ctx;
+        const EVP_MD* evp_md;
+        int md_length;
+        evp_md = EVP_sha256();
+        evp_md_ctx = EVP_MD_CTX_create();
+        EVP_DigestInit_ex(evp_md_ctx, evp_md, NULL);
+        EVP_DigestUpdate(evp_md_ctx, s, strlen(s));
+        EVP_DigestFinal_ex(evp_md_ctx, digest, &md_length);
+        EVP_MD_CTX_destroy(evp_md_ctx);
+        EVP_cleanup();
+        return digest;
 	}
 }
