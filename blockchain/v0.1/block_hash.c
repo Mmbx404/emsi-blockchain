4734c0fcbdfc7fd41ef98a0699443f6bd3eef09d
Mmbx404 <medmehdibelghali@gmail.com>
2021-04-18 23:19:34 +0000

    changes to bloch hash

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -12,7 +12,7 @@ uint8_t *block_hash(block_t const *block,
 		uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
 	size_t len = sizeof(block->info) + block->data.len;
-
+        memset(hash_buf, 0, SHA256_DIGEST_LENGTH);
 	if (!block || !hash_buf)
 		return (NULL);
 
