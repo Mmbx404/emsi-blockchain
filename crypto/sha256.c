a795d4187864cd34afadf7d061bf58e5f58ab28e
Mmbx404 <medmehdibelghali@gmail.com>
2021-04-18 14:40:34 +0000

    another betty style change

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,5 +1,4 @@
 #include "hblk_crypto.h"
-
 /**
  * sha256 - compute the hash of a sequence of bytes
  * @s: Sequence of bytes to be hashed
@@ -8,7 +7,8 @@
  *
  * Return: pointer to digest, NULL on error
  */
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
+uint8_t *sha256(int8_t const *s, size_t len,
+uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	if (!s || !digest)
 		return (NULL);
