b52b604b86a4852d9f21aa5f0c24575870e8818a
Mmbx404 <medmehdibelghali@gmail.com>
2021-05-02 14:29:07 +0000

    block mine

--- blockchain/v0.2/blockchain.h
+++ blockchain/v0.2/blockchain.h
@@ -140,5 +140,6 @@ llist_t *deserialize_blocks(int fd, uint32_t size, uint8_t endianness);
 int block_is_valid(block_t const *block, block_t const *prev_block);
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 uint32_t difficulty);
+void block_mine(block_t *block);
 
 #endif
